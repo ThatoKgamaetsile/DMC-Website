11d69047ee26d1a3ed624726bb67902cfaa84da9
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-02-03 16:15:00 +0200

    new edits

--- se-dmc-develop/se_dmc/views/about.py
+++ se-dmc-develop/se_dmc/views/about.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
-from ..models.team import Team
+# from ..models.team import Team
+from ..models import Team, Testimonial
 
 
 class About(TemplateView):
@@ -8,4 +9,12 @@ class About(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(About, self).get_context_data(**kwargs)
         context['team'] = Team.objects.all()[:5]
+        return context
+
+class Testimonals(TemplateView):
+    template_name = 'se_dmc/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Testimonals, self).get_context_data(**kwargs)
+        context['testimonials'] = Testimonial.objects.all()[:5]
         return context
